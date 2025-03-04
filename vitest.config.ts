ab615267a2aeea5a9d64830d8ed6b08e70a236b3
Jamie Dubs <jamie@jamiedubs.com>
2025-03-04 08:22:25 -0500

    Ignore build/ and dist/ when running (non-existent) tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, configDefaults } from "vitest/config";
 
 export default defineConfig({
   test: {
     globals: true,
     testTimeout: 5_000,
+    exclude: [...configDefaults.exclude, "build/*", "dist/*"],
   },
 });
